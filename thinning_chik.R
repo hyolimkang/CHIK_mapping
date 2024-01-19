7804ddfe29cf1ba246cf199738b2d0d6dc4dc156
hyolim93 <hyolim93@gmail.com>
2024-01-19 21:15:28 +0900

    Update thinning_chik.R

--- thinning_chik.R
+++ thinning_chik.R
@@ -1,25 +1,3 @@
-thin_bg_chik_prac <- function(occ){
-  trv <- 1:length(as.vector(template)) # unique pixel values
-  trv = trv * !is.na(as.vector(template))
-  pixelID = template
-  values(pixelID) = trv
-  
-  bg <- data.frame(Longitude = runif(nrow(occ) * 100, min = -180, max = 180),
-                   Latitude = runif(nrow(occ) * 100, min = -59, max = 75))
-  
-  # trim to those on land and trim to size of occurrence data
-  onland <- !is.na(raster::extract(template, bg[, 1:2]))
-  bg = bg[onland, ]
-  
-  # thinning
-  bg$uID <- raster::extract(pixelID, bg[, 1:2])
-  # remove duplicates
-  bg = bg[!duplicated(bg$uID), ]
-  
-  # trim to size of occurrence dataset
-  bg = bg[1:nrow(occ), ]
-  return(bg)
-} 
 
 ## does this function checks whether the bg points overlap with current occurrence or FOI data locations?
 ## how to find out true absence points?
@@ -60,46 +38,11 @@ thin_bg_chik <- function(occ){
   # remove duplicates
   bg = bg[!duplicated(bg$uID), ]
   
-  # trim to size of occurrence dataset
+  # trim to size of 10 times of FOI dataset
   
   if (nrow(bg) >= nrow(chik_occ)) {
-    bg = bg[1:nrow(chik_occ), ]
+    bg = bg[1:nrow(chik_foi), ]
   }
 } 
 
 
-trv <- 1:length(as.vector(template)) # unique pixel values
-trv = trv * !is.na(as.vector(template))
-pixelID = template
-values(pixelID) = trv
-
-chik_occ_ras <- chik_occ
-
-bg <- data.frame(Longitude = runif(nrow(chik_occ) * 100, min = -180, max = 180),
-                 Latitude = runif(nrow(chik_occ) * 100, min = -59, max = 75))
-
-coordinates(chik_occ_ras) <- ~ long + lat
-crs(chik_occ_ras) <- crs(template)
-occRaster <- rasterize(chik_occ_ras, template, fun=function(...) 1, background=NA)
-plot(occRaster)
-points(bg, col = 'red', pch = 20)
-plot(occRaster, col=c('white', 'blue'), legend=FALSE)
-points(chik_occ_ras, col='red', pch=20, cex=0.5)
-
-
-# trim to those on land and trim to size of occurrence data
-onland <- !is.na(raster::extract(template, bg[, 1:2]))
-bg = bg[onland, ]
-
-# Exclude points in occurrence areas
-notInOcc <- is.na(raster::extract(occRaster, bg[, 1:2]))
-bg = bg[notInOcc, ]
-
-
-# thinning
-bg$uID <- raster::extract(pixelID, bg[, 1:2])
-# remove duplicates
-bg = bg[!duplicated(bg$uID), ]
-if (nrow(bg) >= nrow(chik_occ)) {
-  bg = bg[1:nrow(chik_occ), ]
-}
